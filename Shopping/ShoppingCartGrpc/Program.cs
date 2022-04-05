0b4927123186729048a36db8f8ba758dcba50183
Paulo Pitte <paulopitte@gmail.com>
2022-04-05 12:22:24 -0300

    Complete project

--- Shopping/ShoppingCartGrpc/Program.cs
+++ Shopping/ShoppingCartGrpc/Program.cs
@@ -1,28 +1,49 @@
 using DiscountGrpc.Protos;
 using Microsoft.EntityFrameworkCore;
 using ShoppingCartGrpc.Data;
+using ShoppingCartGrpc.Mapper;
 using ShoppingCartGrpc.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Additional configuration is required to successfully run gRPC on macOS.
-// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
-
-// Add services to the container.
-builder.Services.AddGrpc();
-
-builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>
-    (o => o.Address = new Uri(builder.Configuration.GetValue<string>("GrpcConfigs:DiscountUrl")));
-
+builder.Services.AddScoped<DiscountService>();
 
 
 builder.Services.AddDbContext<ShoppingCartContext>(op =>
 {
     op.UseInMemoryDatabase("ShoppingDb");
 });
+
+builder.Services.AddGrpc(opt =>
+{
+    opt.EnableDetailedErrors = true;
+});
+
+builder.Services.AddAutoMapper(typeof(ShoppingCartProfile));
+
+builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>
+    (o => o.Address = new Uri(builder.Configuration.GetValue<string>("GrpcConfigs:DiscountUrl")));
+
+
+builder.Services.AddAuthentication("bearer")
+    .AddJwtBearer("Bearer", options =>
+    {
+        options.Authority = "https://localhost:5005";
+        options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters()
+        {
+            ValidateAudience = false,
+        };
+    });
+
+builder.Services.AddAuthorization();
+
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.MapGrpcService<ShoppingCartService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
